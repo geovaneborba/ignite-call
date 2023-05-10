a45f71e2307491249733b696b6a3dfbb1f96ca00
Geovane Borba <geovanne@live.com>
2023-05-10 17:16:24 -0300

    refactor: add short weekday names to the list of weekday names

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,18 +1,27 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
-  // [0, 1, 2, 3, 4, 5, 6]
+  /**
+   * Cria um array com 7 posições [0, 1, 2, ...]
+   * logo mapeia cada index retornando os dias da semana
+   * ['segunda-feira', 'terça-feira', ...]
+   * caso seja passado o parâmetro short como true ele vai
+   * retornar [SEG, TER, QUA, ...]
+   */
   const daysOfWeek = Array.from(Array(7).keys())
+    .map((day) => formatter.format(new Date(Date.UTC(2023, 7, day))))
+    .map((weekDay) => {
+      if (short) {
+        // [SEG, TER, ...]
+        return weekDay.substring(0, 3).toLocaleUpperCase()
+      }
+      // ['Segunda-feira', 'Terça-feira', ...]
+      return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
+    })
 
-  // ['segunda-feira', 'terça-feira', ...]
-  const formattedDaysOfWeek = daysOfWeek.map((day) =>
-    formatter.format(new Date(Date.UTC(2023, 7, day)))
-  )
-
-  // ['Segunda-feira', 'Terça-feira', ...]
-  const weekDaysWithFistLetterUppercase = formattedDaysOfWeek.map((weekDay) => {
-    return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
-  })
-
-  return weekDaysWithFistLetterUppercase
+  return daysOfWeek
 }

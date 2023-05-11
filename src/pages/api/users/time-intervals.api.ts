39d046a6bbbe34dd299f78e0bf85d22ad39341dc
Geovane Borba <geovanne@live.com>
2023-05-10 21:27:37 -0300

    chore: remove unnecessary comment

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -34,25 +34,12 @@ export default async function handler(
 
   const { intervals } = timeIntervalsBodySchema.parse(req.body)
 
-  // await Promise.all(
-  //   intervals.map((interval) => {
-  //     return prisma.userTimeInterval.create({
-  //       data: {
-  //         week_day: interval.weekDay,
-  //         time_start_in_minutes: interval.startTimeInMinutes,
-  //         time_end_in_minutes: interval.endTimeInMinutes,
-  //         user_id: session.user.id,
-  //       },
-  //     })
-  //   })
-  // )
-
   await prisma.userTimeInterval.createMany({
     data: intervals.map((interval) => {
       return {
         week_day: interval.weekDay,
         time_start_in_minutes: interval.startTimeInMinutes,
-        time_end_in_minutes: interval.startTimeInMinutes,
+        time_end_in_minutes: interval.endTimeInMinutes,
         user_id: session.user.id,
       }
     }),
